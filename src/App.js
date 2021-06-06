1ca505779fd093916d8f8156ae7b7991dc2cbd18
Safiamoon <msellek.safia@gmail.com>
2021-06-06 18:08:58 +0200

    done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const SEARCHAPI =
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(()=>{
     fetch(APIURL)
@@ -18,8 +19,32 @@ function App() {
       });
   },[]);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+
+      fetch(SEARCHAPI+searchTerm)
+      .then(res => res.json())
+      .then((data) => {
+          console.log(data);
+          setMovies(data.results);
+        });
+        setSearchTerm('');
+    }
+
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
     <>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input className="search" type="text" value={searchTerm} placeholder="Search..." onChange={handleOnChange}></input>
+      </form>
+    </header>
     <div className="movie-container">
       {movies.length > 0 && movies.map(movie => (
         <Movie key= {movie.id} {...movie}/> 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,8 @@ import React from 'react'
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+ 
+
 const Movie = ({title, poster_path, overview, vote_average}) => {
     return (
         <div className="movie">
@@ -11,8 +13,12 @@ const Movie = ({title, poster_path, overview, vote_average}) => {
                 <img src={IMGPATH + poster_path} alt={title}></img>
                 <div className="movie-info">
                     <h3>{title}</h3>
-                    <span>{vote_average}</span>
+                    <span className="tag">{vote_average}</span>
                 </div>
+            </div>
+            <div className="movie-over">
+                <h2>Overview</h2>
+                <p>{overview}</p>
             </div>
         </div>
     )
